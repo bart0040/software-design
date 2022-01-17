84e244cd0cd9d8ebe01fd95e46e06ec4024e834d
Jaap-Jan <bart0040@hz.nl>
2022-01-17 11:18:05 +0100

    first commit

--- src/com/hz/CheckersSetupCreator.java
+++ src/com/hz/CheckersSetupCreator.java
@@ -1,4 +1,18 @@
 package com.hz;
 
 public class CheckersSetupCreator extends GameSetupCreator{
+    @override
+    Pawns[] getPawns() {
+        return new Panws[0];
+    }
+
+    @override
+    Rules[] getRules() {
+        return new Rules[0];
+    }
+
+    @override
+    Board[] getBoard() {
+        return new CheckersBoard();
+    }
 }
--- src/com/hz/ChessSetupCreator.java
+++ src/com/hz/ChessSetupCreator.java
@@ -1,4 +1,19 @@
 package com.hz;
 
-public class ChessSetupCreator {
+public class ChessSetupCreator extends GameSetupCreator{
+
+    @override
+    Pawns[] getPawns() {
+        return new Panws[0];
+    }
+
+    @override
+    Rules[] getRules() {
+        return new Rules[0];
+    }
+
+    @override
+    Board[] getBoard() {
+        return new ChessBoard();
+    }
 }
